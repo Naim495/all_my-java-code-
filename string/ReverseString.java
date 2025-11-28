b7f89659040570c2ecd964b50e8c642f48c64d2f
Naimur Rahman Emon <016112naimrahman@gmail.com>
2025-11-28 19:43:05 +0600

    Create ReverseString.java

---
+++ string/ReverseString.java
@@ -0,0 +1,7 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String s = "hello";
+        String reversed = new StringBuilder(s).reverse().toString();
+        System.out.println(reversed);
+    }
+}
